6604edf5becab364dc5e51729a12a991b7a83002
rickkorsten <rickkorsten@live.nl>
2018-10-04 14:38:25 +0200

    fixed the message model and route

--- sparkles-api/api/models/Message.js
+++ sparkles-api/api/models/Message.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const Message = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     sender : String,
-    message : String,
-    //date_send : String,
-    //relation_id : Number
+    message  : String, 
+    date_send : Date,
+    relation_id : Number,
 })
 module.exports = mongoose.model('Message', Message)
--- sparkles-api/api/routes/message.js
+++ sparkles-api/api/routes/message.js
@@ -8,16 +8,13 @@ const Message = require('../models/Message');
 router.get('/', (req, res, next) => {
 
 	Message.find()
-	.then(message => {
-		res.json({
-			confirmation: 'gelukt',
-			data: message
-		})
+	.exec()
+	.then(docs => {
+		res.status(200).json(docs)
 	})
 	.catch(err => {
-		res.json({
-			confirmation: 'mislukt',
-			data: err.message
+		res.status(500).json({
+			error: err
 		})
 	})
 
@@ -29,26 +26,75 @@ router.post('/', (req, res, next) =>{
 		_id: new mongoose.Types.ObjectId(),
 		sender: req.body.sender,
 		message: req.body.message,
-		//date_send: new Date(),
+		date_send: new Date(),
 		relation_id: req.body.relation_id,
 	})
-	message.save().then(result => {
-		console.log(result)
+
+	message
+	.save()
+	.then(result => {
+		res.status(201).json({
+			message: 'handling POST',
+			createdMessage: result
+		})
 	})
-	.catch(err => console.log(err));
-	// Message.create(req.body)
-	// .then(message => {
-	// 	res.json({
-	// 		confirmation: 'succes',
-	// 		data: message
-	// 	})
-	// })
-	// .catch(err => {
-	// 	res.json({
-	// 		confirmation: 'mislukt',
-	// 		data: err.message
-	// 	})
-	// })
+	.catch(err => {
+		res.status(500).json({
+			error: err
+		})
+	});
+})
+
+router.get('/:messageId', (req, res, next) => {
+	const id = req.params.messageId
+
+	Message
+	.findById(id)
+	.exec()
+	.then(doc => {
+		if (doc) {
+			res.status(200).json(doc)
+		} else {
+			res.status(404).json({message: 'no valid entry found for provided ID'})
+		}
+	})
+	.catch(err => {
+		res.status(500).json({error : err})
+	})
+
+})
+
+router.delete('/:messageId', (req, res, next) => {
+	const id = req.params.messageId
+	const updateOps = {}
+
+	for(const ops of req.body){
+		updateOps[ops.propName] = ops.value
+	}
+
+	Message.update({ _id: id}, { $set: {message: req.body.newMessage } })
+	.exec()
+	.then(result => {
+			res.status(200).json({result})
+	})
+	.catch(err => {
+		res.status(500).json({error : err})
+	})
+
+})
+
+router.patch('/:messageId', (req, res, next) => {
+	const id = req.params.messageId
+
+	Message.remove({ _id: id})
+	.exec()
+	.then(result => {
+			res.status(200).json(result)
+	})
+	.catch(err => {
+		res.status(500).json({error : err})
+	})
+
 })
 
 module.exports = router
