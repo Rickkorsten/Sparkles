1a0ab3c6ecd0d0f48d26d62288ebefb9f2d23d33
Tom Gielen <tomgielen007@gmail.com>
2018-10-10 13:46:45 +0200

    Added a picker to the react app for user selection

--- sparkles/App.js
+++ sparkles/App.js
@@ -4,19 +4,27 @@ import { createBottomTabNavigator } from 'react-navigation';
 import Icon from 'react-native-ionicons'
 
 import HomeScreen from './screens/HomeScreen/HomeScreen'
+import UserScreen from './screens/UserScreen/UserScreen'
 import ChatsScreen from './screens/ChatsScreen/ChatsScreen'
 
 export default createBottomTabNavigator({
+  User: { screen: UserScreen,
+    navigationOptions:{
+      tabBarLabel: 'Account',
+      tabBarIcon: ({tintColor}) => (
+       <Icon ios="ios-person" android="md-person" size={24} />
+      )
+    }},
   Home: { screen: HomeScreen,
    navigationOptions:{
-     tabBarLabel: 'Home',
+     tabBarLabel: 'Spark',
      tabBarIcon: ({tintColor}) => (
       <Icon ios="ios-home" android="md-home" size={24} />
      )
    }},
   Chats: { screen: ChatsScreen,
     navigationOptions:{
-      tabBarLabel: 'Chat',
+      tabBarLabel: 'Sparks',
       tabBarIcon: ({tintColor}) => (
        <Icon ios="ios-chatboxes" android="ios-chatboxes" size={24} />
       )
