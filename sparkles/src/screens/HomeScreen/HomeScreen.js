1abfe0e906fce45a8a7c6f618c86ce21d9bd37df
Tom Gielen <tomgielen007@gmail.com>
2018-10-14 20:00:51 +0200

    Added Ricks match button, styled it and added new branding

--- sparkles/src/screens/HomeScreen/HomeScreen.js
+++ sparkles/src/screens/HomeScreen/HomeScreen.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import LottieView from 'lottie-react-native';
 
+const width = '80%';
+const height = '80%';
 
 class HomeScreen extends Component {
 
@@ -14,17 +16,26 @@ class HomeScreen extends Component {
     console.log(this.props.activeUser)
     return (
       <View style={styles.container}>
-      <LottieView
-        style={{
-          width: 100
-        }}
-        source={require('./../../../assets/animation-searching.json')}
-        autoPlay
-        loop
-      />
-        <Text style={styles.underline}>
-        Looking for the{"\n"}
-        perfect Spark</Text>
+      <Text style={styles.logo}>Sparkles</Text>
+        <View style={styles.sparkContainer}>
+          <LottieView
+            style={{
+              width: 100
+            }}
+            source={require('./../../../assets/animation-searching.json')}
+            autoPlay
+            loop
+          />
+          <Text style={styles.underline}>
+          Looking for the{"\n"}
+          perfect Spark</Text>
+
+          <View>
+          <TouchableOpacity style={styles.matchButton}>
+            <Text style={styles.matchButtonText}>Start a new Spark</Text>
+          </TouchableOpacity>
+        </View>
+        </View>
       </View>
     );
   }
@@ -34,12 +45,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  sparkContainer: {
+    width: '80%',
+    height: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+  logo: {
+    marginBottom: 30,
+    fontFamily: 'Raleway-Light',
+    fontSize: 32,
+    textAlign: 'center'
   },
   underline: {
     marginTop: 20,
     fontFamily: 'Raleway-Light',
     fontSize: 22,
+    textAlign: 'center'
+  },
+  matchButton: {
+    textAlign: 'center',
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 28,
+    paddingRight: 28,
+    borderRadius: 16,
+    borderTopRightRadius: 4,
+    backgroundColor: '#f19894',
+    marginBottom: -60,
+    marginTop: 100
+  },
+  matchButtonText: {
+    color: '#fff',
+    fontFamily: 'Raleway-Light',
+    fontSize: 18,
     textAlign: 'center'
   }
 
