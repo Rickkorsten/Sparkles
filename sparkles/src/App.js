99ec48c0c0094c95f7b818c26e92a28732443ebc
Tom Gielen <tomgielen007@gmail.com>
2018-10-14 19:13:54 +0200

    Tabbar updated, added styling, added animations with Lottie, chat improvments, added fonts

--- sparkles/src/App.js
+++ sparkles/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation';
 import Icon from 'react-native-ionicons'
 // import redux
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // reducer
 import reducers from './reducers';
+import { Animated, Easing } from 'react-native';
+import LottieView from 'lottie-react-native';
 
 import HomeScreen from './screens/HomeScreen/HomeScreen'
 import UserScreen from './screens/UserScreen/UserScreen'
 import ChatsScreen from './screens/ChatsScreen/ChatsScreen'
 
-const Rootstack = createBottomTabNavigator({
+
+const Rootstack = createBottomTabNavigator(
+  {
   User: {
     screen: UserScreen,
     navigationOptions: {
       tabBarLabel: 'Account',
       tabBarIcon: ({ tintColor }) => (
-        <Icon ios="ios-person" android="md-person" size={24} />
+        <Icon ios="md-person" color={tintColor} android="md-person" size={24} />
       )
     }
   },
@@ -27,7 +31,7 @@ const Rootstack = createBottomTabNavigator({
     navigationOptions: {
       tabBarLabel: 'Spark',
       tabBarIcon: ({ tintColor }) => (
-        <Icon ios="ios-home" android="md-home" size={24} />
+        <Icon ios="md-home" color={tintColor} android="md-home" size={24} />
       )
     }
   },
@@ -36,13 +40,35 @@ const Rootstack = createBottomTabNavigator({
     navigationOptions: {
       tabBarLabel: 'Sparks',
       tabBarIcon: ({ tintColor }) => (
-        <Icon ios="ios-chatboxes" android="ios-chatboxes" size={24} />
+        <Icon ios="md-chatbubbles" color={tintColor} android="md-chatbubbles" size={24} />
       )
     }
   }
-});
+}, {
+  tabBarOptions: {
+    activeTintColor: '#f19894',
+    showLabel: false
+  }
+}
+);
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(this.state.progress, {
+      toValue: 1,
+      duration: 5000,
+      easing: Easing.linear,
+    }).start();
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
@@ -57,6 +83,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
 });
 
--- sparkles/src/screens/HomeScreen/HomeScreen.js
+++ sparkles/src/screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
+import LottieView from 'lottie-react-native';
+
 
 class HomeScreen extends Component {
 
@@ -12,7 +14,17 @@ class HomeScreen extends Component {
     console.log(this.props.activeUser)
     return (
       <View style={styles.container}>
-        <Text>{this.props.activeUser.firstName}</Text>
+      <LottieView
+        style={{
+          width: 100
+        }}
+        source={require('./../../../assets/animation-searching.json')}
+        autoPlay
+        loop
+      />
+        <Text style={styles.underline}>
+        Looking for the{"\n"}
+        perfect Spark</Text>
       </View>
     );
   }
@@ -23,7 +35,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  underline: {
+    marginTop: 20,
+    fontFamily: 'Raleway-Light',
+    fontSize: 22,
+    textAlign: 'center'
   }
+
 });
 
 const mapStateToProps = state => {
